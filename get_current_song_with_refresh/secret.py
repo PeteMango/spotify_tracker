74d62c0d79aa9ac5cdbaea32cbc0a7a6697c3755
Peter Wang <40186116+PeteMango@users.noreply.github.com>
2023-01-03 21:37:27 -0500

    Update secret.py

--- get_current_song_with_refresh/secret.py
+++ get_current_song_with_refresh/secret.py
@@ -1,7 +1,7 @@
 import base64
-client_id = 'a2eac0acaaa34f6ab11095c0eac75662'
-client_secret = 'b0038a3ad4da47fdad2efcb4e608132d'
-refresh_token = 'AQCkq9H7LLP-kzUI9SUV9M0RqfLid9tv9W_9kA_KEERV_OjJfPiRLS5vjxDCiH90So7P3cDpA2mY4KMAxX6gKSGvSizSWPNyhmMDStoKaudf0tbuIww86adasuhdBF6I4ac'
+client_id = 
+client_secret = 
+refresh_token = 
 spotify_user_name = "PeteMango"
 
 message = f"{client_id}:{client_secret}"
